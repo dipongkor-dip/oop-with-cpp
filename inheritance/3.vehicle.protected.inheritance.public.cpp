20c08a9185967cae37648ca2f42abfdbee8e042c
dipongkorroy000 <dipongkorroy000@gmail.com>
2026-08-04 09:01:32 +0600

    class useability like public to protected learn

---
+++ inheritance/3.vehicle.protected.inheritance.public.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Vehicle
+{
+public:
+    string name;
+    int noOfTyres;
+
+protected:
+    string model;
+
+public:
+    Vehicle(string v_name, string v_model, int v_noOfTyres)
+    {
+        cout << "Inside Vehicle constructor" << endl;
+        this->name = v_name;
+        this->model = v_model;
+        this->noOfTyres = v_noOfTyres;
+    }
+
+    void start_engine()
+    {
+        cout << "Engine is starting " << name << " " << model << endl;
+    }
+
+    void stop_engine()
+    {
+        cout << "Engine is stoping " << name << " " << model << endl;
+    }
+};
+
+class Car : public Vehicle
+{
+public:
+    int noOfDoors;
+    string transmissionType;
+
+    Car(string v_name, string v_model, int v_noOfTyres, int c_noOfDoors, string c_transmissionType) : Vehicle(v_name, v_model, v_noOfTyres)
+    {
+        cout << "Inside Car constructor" << endl;
+        this->noOfDoors = c_noOfDoors;
+        this->transmissionType = c_transmissionType;
+    }
+
+    void startAC()
+    {
+        cout << "AC has started of " << model << endl;
+    }
+};
+
+int main()
+{
+    Car car("Honda Car", "LXI", 4, 4, "Manual");
+
+    car.start_engine();
+
+    car.startAC();
+
+    car.start_engine();
+
+    return 0;
+}
+
+/**
+ * output->
+ * Inside Vehicle constructor
+ * Inside Car constructor
+ * Engine is starting Honda Car LXI
+ * AC has started of Honda Car
+ * Engine is starting Honda Car LXI
+ */
---
+++ inheritance/6.vehicle.protected.inheritance.protected.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Vehicle
+{
+protected:
+    string name;
+    int noOfTyres;
+    string model;
+
+public:
+    Vehicle(string v_name, string v_model, int v_noOfTyres)
+    {
+        cout << "Inside Vehicle constructor" << endl;
+        this->name = v_name;
+        this->model = v_model;
+        this->noOfTyres = v_noOfTyres;
+    }
+
+    void start_engine()
+    {
+        cout << "Engine is starting " << name << " " << model << endl;
+    }
+
+    void stop_engine()
+    {
+        cout << "Engine is stoping " << name << " " << model << endl;
+    }
+};
+
+class Car : protected Vehicle
+{
+    // vehicle attributes are protected
+public:
+    int noOfDoors;
+    string transmissionType;
+
+    Car(string v_name, string v_model, int v_noOfTyres, int c_noOfDoors, string c_transmissionType) : Vehicle(v_name, v_model, v_noOfTyres)
+    {
+        cout << "Inside Car constructor" << endl;
+        this->noOfDoors = c_noOfDoors;
+        this->transmissionType = c_transmissionType;
+    }
+
+    void startAC()
+    {
+        cout << "AC has started of " << model << endl;
+    }
+
+    void start_engine_by_car()
+    {
+        start_engine();
+    }
+
+    void stop_engine_by_car()
+    {
+        stop_engine();
+    }
+};
+
+int main()
+{
+    Car car("Honda Car", "LXI", 4, 4, "Manual");
+
+    // car.start_engine(); inaccessible
+    car.start_engine_by_car();
+
+    car.startAC();
+
+    // car.start_engine(); inaccessible
+    car.stop_engine_by_car();
+
+    return 0;
+}
+
+/**
+ * output->
+ * Inside Vehicle constructor
+ * Inside Car constructor
+ * Engine is starting Honda Car LXI
+ * AC has started of Honda Car
+ * Engine is starting Honda Car LXI
+ */
